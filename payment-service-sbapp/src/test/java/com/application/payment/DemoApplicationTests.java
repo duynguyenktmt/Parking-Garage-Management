8c5d7bfbd25b8bb1a3837248fd00cdcd062f1368
Dustin Nguyen <duynguyenktmt05@gmail.com>
2024-10-24 02:53:02 +0000

    setup env for service modules...

---
+++ payment-service-sbapp/src/test/java/com/application/payment/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.application.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = {DemoApplication.class})
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
